f6c934ce487f0ae8be756a30769d58022aba7f6d
Alyssa Hernandez <alyssa@artivity.co>
2021-03-27 16:50:57 -0400

    First commit

---
+++ dev-website/src/test/java/com/alyssa/devwebsite/DevWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alyssa.devwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
